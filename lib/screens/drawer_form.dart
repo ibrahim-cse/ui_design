4262b659fcb5588956455521a6985db82e44bbb7
ibrahim-cse <ihrahad233@gmail.com>
2021-12-12 00:03:05 +0600

    Appbar added

--- lib/screens/drawer_form.dart
+++ lib/screens/drawer_form.dart
@@ -11,6 +11,9 @@ class _DrawerFormState extends State<DrawerForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Drawer'),
+      ),
       body: SafeArea(
         child: Text('Drawer Page'),
       ),
