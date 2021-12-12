9fe18f56583103eaf6b6da5351efd759f8770e37
ibrahim-cse <ihrahad233@gmail.com>
2021-12-12 10:55:39 +0600

    Fixed Renderflex.

--- lib/screens/login_form.dart
+++ lib/screens/login_form.dart
@@ -32,143 +32,140 @@ class _LoginFormState extends State<LoginForm> {
           child: Scaffold(
             backgroundColor: Colors.transparent,
             body: Center(
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 50.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/loginLogo.jpg'),
-                      ),
-                    ),
-                    height: 150.0,
-                    width: 150.0,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/spectrumLogo.jpg'),
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    height: 80.0,
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextField(
-                      style: const TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                      controller: username,
-                      decoration: const InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 50.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/loginLogo.jpg'),
                         ),
-                        labelText: "Username",
-                        labelStyle:
-                            TextStyle(fontSize: 18, color: Colors.white),
-                        // hintText: "Enter your email",
-                        hintStyle: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.white),
                       ),
+                      height: 150.0,
+                      width: 150.0,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextField(
-                      style: const TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                      controller: username,
-                      decoration: const InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/spectrumLogo.jpg'),
+                          fit: BoxFit.contain,
                         ),
-                        labelText: "Password",
-                        labelStyle:
-                            TextStyle(fontSize: 18, color: Colors.white),
-                        // hintText: "Enter your password",
-                        hintStyle: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.white),
                       ),
+                      height: 80.0,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      width: double.infinity,
-                      height: 50.0,
-                      child: FlatButton(
-                        color: Colors.blue,
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DrawerForm()),
-                          );
-                        },
-                      ),
+                    SizedBox(
+                      height: 30.0,
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Radio(
-                          value: 1,
-                          activeColor: Colors.white,
-                          groupValue: false,
-                          onChanged: (value) {
-                            setState(() {
-                              // _value = value as int;
-                            });
-                          }),
-                      const Text(
-                        'Remember Password',
-                        style: TextStyle(fontSize: 17.0, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: RichText(
-                            text: const TextSpan(
-                              text:
-                                  'By downloading and using the application, you aggree to the ',
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 16.0),
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text:
-                                        'Terms of the (EULA) agreement Privacy Policy.',
-                                    style: TextStyle(
-                                        color: Colors.amber, fontSize: 16.0)),
-                              ],
-                            ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextField(
+                        style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                        controller: username,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
                           ),
+                          labelText: "Username",
+                          labelStyle:
+                              TextStyle(fontSize: 18, color: Colors.white),
+                          // hintText: "Enter your email",
+                          hintStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextField(
+                        style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                        controller: username,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                          labelText: "Password",
+                          labelStyle:
+                              TextStyle(fontSize: 18, color: Colors.white),
+                          // hintText: "Enter your password",
+                          hintStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        width: double.infinity,
+                        height: 50.0,
+                        child: FlatButton(
+                          color: Colors.blue,
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DrawerForm()),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Radio(
+                            value: 1,
+                            activeColor: Colors.white,
+                            groupValue: false,
+                            onChanged: (value) {
+                              setState(() {
+                                // _value = value as int;
+                              });
+                            }),
+                        const Text(
+                          'Remember Password',
+                          style: TextStyle(fontSize: 17.0, color: Colors.white),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: RichText(
+                        text: const TextSpan(
+                          text:
+                              'By downloading and using the application, you aggree to the ',
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text:
+                                    'Terms of the (EULA) agreement Privacy Policy.',
+                                style: TextStyle(
+                                    color: Colors.amber, fontSize: 16.0)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0S,
+                    )
+                  ],
+                ),
               ),
             ),
           ),
